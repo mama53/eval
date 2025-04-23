a1b7863deea032f6b773eac67e06f5982c704a36
Mathys Cottin <iiamathysexample.com>
2025-04-23 16:57:43 +0200

    Même prblm que linux 1 modification effectuée

--- linux2/linux2server-info.py
+++ linux2/linux2server-info.py
@@ -1,10 +1,11 @@
 import psutil
 
 # Get the memory usage information
-memory = psutil.virtualMemory()
+memory = psutil.virtualMemory() 
 
 # Print the memory usage details
 print("Total Memory: {:.2f} GB".format(memory.total / (1024 ** 3)))
 print("Available Memory: {:.2f} GB".format(memory.available / (1024 ** 3)))
-print("Used Memory: {:.2f} GB".format(memory.available / (1024 ** 3)))
+print("Used Memory: {:.2f} GB".format(memory.used / (1024 ** 3)))  # Correction ici du même probleme constater 
 print("Memory Percent: {:.2f}%".format(memory.percent))
+
